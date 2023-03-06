538335c90fed632806dd1be4b438f849102662ab
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-03-06 08:55:13 +0530

    2- Writing Domain Layer

---
+++ app/src/main/java/com/kuluruvineeth/exptracker/domain/models/Account.kt
@@ -0,0 +1,8 @@
+package com.kuluruvineeth.exptracker.domain.models
+
+data class Account(
+    val account: String,
+    val amount: Double,
+    val income: Double,
+    val expense: Double
+)
---
+++ app/src/main/java/com/kuluruvineeth/exptracker/domain/repository/DataStoreRepository.kt
@@ -0,0 +1,28 @@
+package com.kuluruvineeth.exptracker.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+
+interface DataStoreRepository {
+
+    suspend fun writeOnboardingKeyToDatastore(completed: Boolean)
+
+    suspend fun readOnboardingKeyFromDataStore(): Flow<Boolean>
+
+    suspend fun writeCurrencyToDatastore(currency: String)
+
+    suspend fun readCurrencyFromDatastore(): Flow<String>
+
+    suspend fun writeExpenseLimitToDatastore(amount: Double)
+
+    suspend fun readExpenseLimitFromDatastore(): Flow<Double>
+
+    suspend fun writeLimitKeyToDatastore(enabled: Boolean)
+
+    suspend fun readLimitKeyFromDatastore(): Flow<Boolean>
+
+    suspend fun writeLimitDurationToDatastore(duration: Int)
+
+    suspend fun readLimitDurationFromDatastore(): Flow<Int>
+
+    suspend fun eraseDatastore()
+}
